63e350cbc3f02e4cd38d76f41a7b8bba0307bc4c
lxia0034@student.monash.edu <lxia0034@student.monash.edu>
2025-08-22 18:50:54 +1000

    part of the week3

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import JSONLab from './components/JSON.vue'
+import JSON from './components/JSON.vue'
+import Form from './components/Form.vue';
 </script>
 
 <template>
-  <JSONLab />
+  <!-- <JSON /> -->
+  <Form />
 </template>
 
 <style scoped></style>
