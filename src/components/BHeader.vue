6bb4763fc346fbd7f0f4bec788a62ad79e8b35cf
Anhe-zz <xiaanhe705@gmail.com>
2025-09-19 22:42:44 +1000

    Modified redundant UI

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -16,9 +16,6 @@
           <router-link to="/addbook" class="nav-link" active-class="active">Add Book</router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/login" class="nav-link" active-class="active">Login</router-link>
-        </li>
-        <li class="nav-item">
           <router-link to="/FireLogin" class="nav-link" active-class="active">Firebase Login</router-link>
         </li>
         <li class="nav-item">
